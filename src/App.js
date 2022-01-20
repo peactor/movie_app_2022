e107424b51f00b6cb8decc266aae5005e0e839fe
peactor <peactor@gmail.com>
2022-01-20 13:58:42 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+function Food({ fav }){
+  
+  return <h1>I like { fav }</h1>;
+}
+
+
 function App() {
-  return <div>Hello!!</div>;
+  return (
+  <div>
+    <h1>React App</h1>
+    <Food fav="kimchi" />
+    <Food fav="ramen" />
+    <Food fav="samgiopsal" />
+    <Food fav="chukumi" />
+
+    </div>);
 }
 
 export default App;
