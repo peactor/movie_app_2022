e0d7455d854dfd5be023f59d8fdda431589fdb86
peactor <peactor@gmail.com>
2022-01-21 17:34:15 +0900

    lesson5 state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  
   render(){
-    return <h1>I'm a class component</h1>;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 
 }
